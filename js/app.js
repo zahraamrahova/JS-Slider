d0e695241d5f9fe71a309f2c850678db28577c44
zahraamrahova <zehra.khudaverdiyeva@gmail.com>
2019-02-23 23:26:41 +0400

    JS Slider

---
+++ js/app.js
@@ -0,0 +1,58 @@
+(function () {
+    const pictures = [
+        "contBcg-0",
+        "contBcg-1",
+        "contBcg-2",
+        "contBcg-3",
+        "contBcg-4",
+        "contBcg-5",
+    ];
+
+    // setup the counter
+    let counter = 0;
+    //select the buttons
+    const btn = document.querySelectorAll('.btn');
+
+    btn.forEach(function (btn) {
+        btn.addEventListener('click', function (event) {
+            // get event target
+            const value = event.target;
+            //  console.log(value);
+
+
+            if (value.classList.contains("btn-left")) {
+                counter--;
+                if (counter < 0) {
+                    counter = pictures.length - 1;
+                }
+                console.log(counter);
+                document.querySelector('.img-container').style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+            }
+            if (value.classList.contains("btn-right")) {
+                counter++;
+                if (counter > pictures.length - 1) {
+                    counter = 0;
+                }
+                console.log(counter);
+                document.querySelector('.img-container').style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
